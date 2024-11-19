043cd9345083f0afd8c7a435cf76a268fb109b77
evanl127 <144394321+evanl127@users.noreply.github.com>
2024-11-19 15:28:55 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+button1 = document.getElementById("input-btn")
+button1.addEventListener("click", save)
+myLinks = []
+inputEl = ""
+
+function save(){
+    myLinks.push("http://www. ${inputEL}")
+    console.log("Button Clicked!")
+    console.log(myLinks)
+}
